47464e761a241b6e918734a40e48e93a3c876d0c
Diana Estrada <estrada.diana120@gmail.com>
2025-01-28 20:16:11 -0800

    Minor changes to GUI

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -12,6 +12,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
+#include <cmath>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
@@ -203,8 +204,22 @@ int main(int, char**)
             ImGui::Begin("Implot", &show_simple_data_window);
             ImGui::Text("Here are the graphs!");
 
+            static float xs1[1001], ys1[1001];
+            for (int i = 0; i < 1001; ++i) {
+                xs1[i] = i * 0.001f;
+                ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + (float)ImGui::GetTime() / 10));
+            }
+
+            static double xs2[20], ys2[20];
+            for (int i = 0; i < 20; ++i) {
+                xs2[i] = i * 1/19.0f;
+                ys2[i] = xs2[i] * xs2[i];
+            }
             if (ImPlot::BeginPlot("Line Plots")) {
                 ImPlot::SetupAxes("x","y");
+                ImPlot::PlotLine("f(x)", xs1, ys1, 1001);
+                ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+                ImPlot::PlotLine("g(x)", xs2, ys2, 20,ImPlotLineFlags_Segments);
                 ImPlot::EndPlot();
             }
 
